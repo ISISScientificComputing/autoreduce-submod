9e809a4352c2e1642f8178e91661a6860d9a0455
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-06-07 15:59:26 +0100

    Replace submodules with git clone

--- runme.sh
+++ runme.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-git submodule update --init
+git clone https://github.com/ISISScientificComputing/autoreduce-utils
+git clone https://github.com/ISISScientificComputing/autoreduce-frontend
+git clone https://github.com/ISISScientificComputing/autoreduce-db
+git clone https://github.com/ISISScientificComputing/autoreduce-containers
+git clone https://github.com/ISISScientificComputing/autoreduce-actions
+git clone https://github.com/ISISScientificComputing/autoreduce-scripts
+git clone https://github.com/ISISScientificComputing/ansible
+git clone https://github.com/ISISScientificComputing/autoreduce
