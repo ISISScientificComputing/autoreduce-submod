c1399364eb702629b2be6a20150c35b605d549f2
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-06-30 14:06:01 +0100

    Adds autoreduce-notebooks

--- runme.sh
+++ runme.sh
@@ -9,3 +9,4 @@ git clone https://github.com/ISISScientificComputing/autoreduce-scripts
 git clone https://github.com/ISISScientificComputing/ansible
 git clone https://github.com/ISISScientificComputing/autoreduce
 git clone https://github.com/ISISScientificComputing/autoreduce-rest-api
+git clone https://github.com/ISISScientificComputing/autoreduce-notebooks
