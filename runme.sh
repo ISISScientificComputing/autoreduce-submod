6c35284ea229abd4c4682ee52ac357fb0327b16c
rickybas <rickybas12@gmail.com>
2021-08-03 16:58:18 +0100

    Add yapf pre-commit hook

--- runme.sh
+++ runme.sh
@@ -10,3 +10,12 @@ git clone https://github.com/ISISScientificComputing/ansible
 git clone https://github.com/ISISScientificComputing/autoreduce
 git clone https://github.com/ISISScientificComputing/autoreduce-rest-api
 git clone https://github.com/ISISScientificComputing/autoreduce-notebooks
+
+# Setup pre-commit hooks for cloned repositories
+pip install -r requirements.txt
+for d in */; do
+  if [[ $d != *"venv"* ]]; then # skip setting up pre-commit hook in *venv* directory
+    cp -v .pre-commit-config.yaml "$d" && (cd "$d" && pre-commit install)
+  fi
+done
+
