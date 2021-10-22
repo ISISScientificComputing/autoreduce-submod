506e61412440a97f6493abcfd06e5cd6e5b6c160
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-22 14:14:30 +0000

    Adds install script for packages

---
+++ install-in-pip.sh
@@ -0,0 +1,8 @@
+pip install -e autoreduce-frontend
+pip install -e autoreduce-rest-api
+pip install -e autoreduce
+pip install -e autoreduce-scripts
+pip install -e autoreduce-db
+pip install -e autoreduce-utils
+
+pip install -r autoreduce-actions/requirements.txt
