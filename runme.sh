b892c5855d97185e85abb0116182f1509d31db25
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-22 13:31:54 +0000

    Updates

--- runme.sh
+++ runme.sh
@@ -11,11 +11,4 @@ git clone https://github.com/ISISScientificComputing/autoreduce
 git clone https://github.com/ISISScientificComputing/autoreduce-rest-api
 git clone https://github.com/ISISScientificComputing/autoreduce-notebooks
 
-# Setup pre-commit hooks for cloned repositories
-pip install -r requirements.txt
-for d in */; do
-  if [[ $d != *"venv"* ]]; then # skip setting up pre-commit hook in *venv* directory
-    cp -v .pre-commit-config.yaml "$d" && (cd "$d" && pre-commit install)
-  fi
-done
 
